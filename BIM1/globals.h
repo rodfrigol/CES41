e42b245b89d6beacd18fa7b12d33240e0c2256ee
rodfrigol <rodrigoalalmeida@gmail.com>
2022-09-16 20:15:10 -0300

    create lexical analyzer

---
+++ BIM1/globals.h
@@ -0,0 +1,10 @@
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+typedef enum{
+    ELSE, IF, INT, RETURN, VOID, WHILE,
+    PLUS, MINUS, MULT, DIV, LT, LTE, GT, GTE, EQ, DIF, ASSIGN, SEMI, COMMA, OP, CP, OB, CB, OBRACES, CBRACES,
+    NUM, ID,
+    ERROR} TokenType;
+
+#endif
